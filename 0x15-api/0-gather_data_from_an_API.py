d6ec4330fb1092a3fa0b51ebb13eed25abac8ec8
Sebo26 <sebomaduna26@gmail.com>
2024-04-27 11:22:09 -0700

    Starting over

--- 0x15-api/0-gather_data_from_an_API.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import requests
-employee ID must be an integer
-requests.get(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
-requests.post(f"Employee {EMPLOYEE_NAME} is done with tasks{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}: ")
-requests.get(TASK_TITLE)
-requests.post({TASK_TITLE})
